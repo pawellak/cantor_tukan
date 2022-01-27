dd3b2924f611f76457e410211b6ec8f6d42cde6f
Pawel Lak <pawellak@gmail.com>
2022-01-27 17:51:19 +0100

    [Add] auth facade Interface

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:kantor_tukan/domain/auth/auth_failure.dart';
+import 'package:kantor_tukan/domain/auth/value_object.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
